b676e1910742626a983ef7c3e5e5dc040a0082ec
Nicolas Castro <nicolascastro124@gmail.com>
2024-11-06 01:19:38 -0300

    Configuracion vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,6 @@
     <!--estilos / scripts -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-
     <!-- assets compilados por Vite -->
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <br>
