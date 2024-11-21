df59394e7c9fb88e5dc57a36dc4dd95c1e49e067
Nicolas Castro <nicolascastro124@gmail.com>
2024-11-21 05:01:48 -0300

    ventas correciones

--- resources/views/producto/registrar.blade.php
+++ resources/views/producto/registrar.blade.php
@@ -68,4 +68,16 @@
             </tr>
         </table>
     </form>
+
+@if ($errors->any())
+    <div class="alert alert-danger mt-3">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
 @endsection
